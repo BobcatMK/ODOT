565dc53e676a4acb4464f7d171855f7def3816dc
BobcatMK <matthew.kilan@gmail.com>
2014-08-25 18:28:16 +0200

    Added new tests for edit items

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -10,15 +10,37 @@ describe "Adding todo items" do
     end  
   end
   
-  #it "is successful with valid content" do
-  #  visit_todo_list(todo_list)
-  #  click_link "New Todo Item"
-  #  fill_in "Content",with: "Milk"
-  #  click_button "Save"
-  #  expect(page).to(have_content("Added todo list item."))
-  #  within("ul.todo_items") do
-  #    expect(page).to(have_content("Milk"))
-  #  end
-  #end
+  it "is successful with valid content" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content",with: "Milk"
+    click_button "Save"
+    expect(page).to(have_content("Added todo list item."))
+    within("ul.todo_items") do
+      expect(page).to(have_content("Milk"))
+    end
+  end
+  
+  it "displays an error with no content" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: ""
+    click_button "Save"
+    within("div#error_explanation") do
+      expect(page).to(have_content("prohibited this todo item from being saved:"))
+    end
+    expect(page).to(have_content("Content can't be blank"))
+  end
+  
+  it "displays an error with content less than 2 characters" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: "a"
+    click_button "Save"
+    within("div#error_explanation") do
+      expect(page).to(have_content("prohibited this todo item from being saved:"))
+    end
+    expect(page).to(have_content("Content is too short (minimum is 2 characters)"))
+  end
  
 end
